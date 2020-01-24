8bb6090a4d6bc77118c619127af13f4483b5d238
fukugit <fukuma1234@gmail.com>
2020-01-24 23:41:36 +0900

    Move logic actual to test

--- test/6object.js
+++ test/6object.js
@@ -1,8 +1,40 @@
+
+/**
+ * ベースとなるオブジェクト
+ */
+var book = {
+  title: "JavaScript",
+  getTitle: function() {
+    return this.title;
+  }
+}
+
+/**
+ * bookを継承したオブジェクトを作成します。
+ */
+function createObject() {
+  function novel() {
+    this.title = "This is a novel."
+  };
+  // ここでベースオブジェクトを継承します。しかしtitleは子オブジェクトのものが有効になります。
+  novel.prototype = book;
+  return new novel(); // novelオブジェクトのprototypeにはベースオブジェクトが入っているので、ベースのメソッドが使用できます。
+}
+
+/**
+ * bookを継承したオブジェクトから、ベースオブジェクトbookのメソッドを呼び出します。
+ */
+function getNovelTitle() {
+  var f = createObject();
+  return f.getTitle();
+}
+
+/************ Testing by Mohca ************/
+
 var chai = require('chai');
 var assert = chai.assert;
-var func = require('../js/6object.js');
 
 describe('オブジェクトの継承の確認をします。', function () {
   it('ベースオブジェクトのtitleが取得出来ていることを確認します。',
-    function(){assert.strictEqual(func.getNovelTitle(), "This is a novel.") });
+    function(){assert.strictEqual(getNovelTitle(), "This is a novel.") });
 });
