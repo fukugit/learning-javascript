63a15f1a1e26c3983f0ac60b7a0cca2035e9fba9
fukugit <fukuma1234@gmail.com>
2020-01-25 11:27:22 +0900

    Change something

--- test/6object.js
+++ test/6object.js
@@ -1,40 +1,77 @@
-
 /**
- * ベースとなるオブジェクト
+ * サイ本6章、P121
+ * オブジェクトについて理解します。
+ * 1. オブジェクトリテラルの様々なパターンを実施します。
+ * 2. プロトタイプを使ったオブジェクトの継承を実施します。
+ *
  */
-var book = {
-  title: "JavaScript",
-  getTitle: function() {
-    return this.title;
+
+
+/************ 1. オブジェクトリテラルの様々なパターン ************/
+
+
+/************ 2. プロトタイプを使ったオブジェクトの継承 ************/
+/**
+ * 親オブジェクト
+ */
+var japan = {
+  area: "東の小さな国",
+  getArea: function() {
+    return this.area;
   }
 }
 
 /**
- * bookを継承したオブジェクトを作成します。
+ * 子オブジェクト, その１
  */
-function createObject() {
-  function novel() {
-    this.title = "This is a novel."
+function createTokyoObject() {
+  // オブジェクトを継承するために空の関数(コンストラクタ)を用意します。
+  // varで宣言したオブジェクトへは継承できません。（コンストラクタが無いため）
+  // １つの関数には１つのプロトタイプしかないため、継承できるオブジェクトは１階層のみです。（多重階層を継承するにはミックスインが必要）
+  function tokyo() {  // オブジェクトを継承するために空の関数(コンストラクタ)を用意します。
+    this.region = "関東";
   };
-  // ここでベースオブジェクトを継承します。しかしtitleは子オブジェクトのものが有効になります。
-  novel.prototype = book;
-  return new novel(); // novelオブジェクトのprototypeにはベースオブジェクトが入っているので、ベースのメソッドが使用できます。
+  tokyo.prototype = japan; // プロトタイプに親オブジェクトを入れることで、親を継承します。
+  return new tokyo();
 }
 
 /**
- * bookを継承したオブジェクトから、ベースオブジェクトbookのメソッドを呼び出します。
+ * 子オブジェクト, その２
  */
-function getNovelTitle() {
-  var f = createObject();
-  return f.getTitle();
+function createOsakaObject() {
+  function osaka() {
+    this.region = "かんさい";
+  };
+  osaka.prototype = japan;
+  return new osaka();
 }
+
+/**
+ * 親オブジェクトのメソッドを呼び出します。
+ */
+function getTokyoArea() {
+  var tokyo = createTokyoObject();
+  return tokyo.getArea();
+}
+
+/**
+ * 子オブジェクトのフィールドを呼び出します。
+ */
+function getOsakaRegion() {
+  var osaka = createOsakaObject();
+  return osaka.region;
+}
+
 
 /************ Testing by Mohca ************/
 
 var chai = require('chai');
 var assert = chai.assert;
 
-describe('オブジェクトの継承の確認をします。', function () {
-  it('ベースオブジェクトのtitleが取得出来ていることを確認します。',
-    function(){assert.strictEqual(getNovelTitle(), "This is a novel.") });
+describe('6章オブジェクト, 2. プロトタイプを使ったオブジェクトの継承', function () {
+  it('親オブジェクトが継承できていることを確認します。 ',
+    function(){assert.strictEqual(getTokyoArea(), "東の小さな国") });
+  it('子オブジェクトのフィールドが参照できることを確認します。 ',
+    function(){assert.strictEqual(getOsakaRegion(), "かんさい") });
+
 });
