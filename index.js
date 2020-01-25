0b3e9cb7c15870c78f5a033bf0b30f5449312d8c
fukugit <fukuma1234@gmail.com>
2020-01-25 23:16:18 +0900

    Add sample app js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-var math = require('./js/math.js');
-var exampleFunction = require('./js/8function.js');
+var app = require('./js/app.js');
 
-console.log("math : " + math.add(1,3));
-console.log("8function : " + exampleFunction.plus(1,3));
+console.log("plus : " + app.plus(1,2));
---
+++ js/app.js
@@ -0,0 +1,9 @@
+// var plus = function(x,y) {
+//   return x + y;
+// }
+// module.exports.plus = plus;
+//
+function plus(x,y) {
+  return x + y;
+}
+exports.plus = plus;
